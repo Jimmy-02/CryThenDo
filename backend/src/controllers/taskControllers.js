0f378fb7fa520f57d23c32217b2be9ef9d9a7adb
Jimmy-02 <tainat1202@gmail.com>
2026-03-23 15:16:29 +0700

    feat: inital get all tasks api

---
+++ backend/src/controllers/taskControllers.js
@@ -0,0 +1,3 @@
+export const getAllTasks = (req, res) => {
+    req.status(200).send("All tasks");
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
-
+import taskRouters from './routes/taskRouters.js';
 const app = express();
+
+app.use("/api/tasks", taskRouters);
 
 app.listen(5001, () => {
   console.log('Server is running on port 5001');
