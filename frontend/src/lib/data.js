abaa2a890ea0143791ed8f6d76fe6ecbdc322f03
Jimmy-02 <tainat1202@gmail.com>
2026-04-03 17:33:37 +0700

    feat: implement date time filter UI

--- frontend/src/lib/data.js
+++ frontend/src/lib/data.js
@@ -3,3 +3,22 @@ export const FilterType = {
   active: "Active",
   completed: "Completed",
 };
+
+export const options = [
+  {
+    value: "today",
+    label: "Today",
+  },
+  {
+    value: "week",
+    label: "This Week",
+  },
+  {
+    value: "month",
+    label: "This Month",
+  },
+  {
+    value: "all",
+    label: "All",
+  },
+];
