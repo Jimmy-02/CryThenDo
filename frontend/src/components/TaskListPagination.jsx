97a2176b36a7c92490ef785fd11ad4837ad6aa06
Jimmy-02 <tainat1202@gmail.com>
2026-04-03 19:27:18 +0700

    feat: implement pagination

--- frontend/src/components/TaskListPagination.jsx
+++ frontend/src/components/TaskListPagination.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { cn } from "@/lib/utils";
 
-const TaskListPagination = () => {
+const TaskListPagination = ({
+  handleNext,
+  handlePrev,
+  handlePageChange,
+  page,
+  totalPages,
+}) => {
+  const generatePages = () => {
+    if (totalPages <= 4) {
+      return Array.from({ length: totalPages }, (_, i) => i + 1);
+    }
+
+    if (page <= 4) {
+      return [1, 2, 3, 4, 5, "...", totalPages];
+    }
+
+    if (page >= totalPages - 3) {
+      return [
+        1,
+        "...",
+        totalPages - 4,
+        totalPages - 3,
+        totalPages - 2,
+        totalPages - 1,
+        totalPages,
+      ];
+    }
+
+    return [1, "...", page - 1, page, page + 1, "...", totalPages];
+  };
+
+  const pagesToShow = generatePages();
+
+  if (totalPages <= 1) return null;
+
   return (
-    <div>TaskListPagination</div>
-  )
-}
+    <div className="mt-2 flex justify-center">
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              onClick={page === 1 ? undefined : handlePrev}
+              className={cn(
+                "w-24 justify-center cursor-pointer",
+                page === 1 && "pointer-events-none opacity-50",
+              )}
+            />
+          </PaginationItem>
 
-export default TaskListPagination
+          {pagesToShow.map((p, index) => (
+            <PaginationItem key={`${p}-${index}`}>
+              {p === "..." ? (
+                <div className="flex h-10 w-10 items-center justify-center">
+                  <PaginationEllipsis />
+                </div>
+              ) : (
+                <PaginationLink
+                  isActive={p === page}
+                  onClick={() => {
+                    if (p !== page) handlePageChange(p);
+                  }}
+                  className="flex h-10 w-10 cursor-pointer items-center justify-center"
+                >
+                  {p}
+                </PaginationLink>
+              )}
+            </PaginationItem>
+          ))}
+
+          <PaginationItem>
+            <PaginationNext
+              onClick={page === totalPages ? undefined : handleNext}
+              className={cn(
+                "w-24 justify-center cursor-pointer",
+                page === totalPages && "pointer-events-none opacity-50",
+              )}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
+};
+
+export default TaskListPagination;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -8,13 +8,14 @@ import TaskListPagination from '@/components/TaskListPagination';
 import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 import api from '@/lib/axios';
-
+import { visibleTaskLimit } from '@/lib/data';
 const HomePage = () => {
   const [taskBuffer, setTaskBuffer] = useState([]);
   const [activeTaskCount, setActiveTaskCount] = useState(0);
   const [completeTaskCount, setCompleteTaskCount] = useState(0);
   const [filter, setFilter] = useState("all");
   const [dateQuery, setDateQuery] = useState("today");
+  const [page, setPage] = useState(1);
   const fetchTasks = async () => {
     try {
       const res = await api.get(`/tasks?filter=${dateQuery}`);
@@ -32,9 +33,30 @@ const HomePage = () => {
     fetchTasks();
   }, [dateQuery]);
 
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setPage(1);
+  }, [filter, dateQuery]);
+
   const handleTaskChanged = () => {
     fetchTasks();
   };
+  
+  const handleNext = () =>{
+    if(page < totalPages) {
+      setPage((prev) => prev + 1);
+    }
+  };
+
+  const handlePrev = () =>{
+    if(page > 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  }
   const filteredTasks = taskBuffer.filter((task) => {
     switch (filter) {
       case "active":
@@ -45,6 +67,17 @@ const HomePage = () => {
         return true;
     }
   });
+
+  const visibleTasks = filteredTasks.slice(
+    (page - 1) * visibleTaskLimit,
+    page * visibleTaskLimit,
+  );
+  
+  if(visibleTasks.length === 0 && page > 1) {
+    handlePrev();
+  }
+
+  const totalPages = Math.ceil(filteredTasks.length / visibleTaskLimit);
   return (
     <div className="min-h-screen w-full bg-[#fefcff] relative">
       <div
@@ -68,10 +101,16 @@ const HomePage = () => {
             completedTasksCount={completeTaskCount}
           />
 
-          <TaskList filteredTasks={filteredTasks} filter={filter} handleTaskChanged={handleTaskChanged} />
+          <TaskList filteredTasks={visibleTasks} filter={filter} handleTaskChanged={handleTaskChanged} />
 
           <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-            <TaskListPagination />
+            <TaskListPagination 
+              handleNext={handleNext}
+              handlePrev={handlePrev}
+              handlePageChange={handlePageChange}
+              page={page}
+              totalPages={totalPages}
+            />
             <DateTimeFilter dateQuery={dateQuery} setDateQuery={setDateQuery} />
           </div>
 
